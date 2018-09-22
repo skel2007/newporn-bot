a67335d306b13c0a015193dda4f29b780121c8cc
Alexey D <skel2007@gmail.com>
2018-09-22 12:16:17 +0300

    singletons

--- src/main/kotlin/com/github/skel2007/newporn/model/NewpornModelModule.kt
+++ src/main/kotlin/com/github/skel2007/newporn/model/NewpornModelModule.kt
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoDatabase
 import dagger.Module
 import dagger.Provides
 import org.litote.kmongo.KMongo
+import javax.inject.Singleton
 
 /**
  * @author skel
@@ -13,22 +14,26 @@ import org.litote.kmongo.KMongo
 class NewpornModelModule {
 
     @Provides
+    @Singleton
     fun provideMongoClient(): MongoClient {
         val host = System.getenv("MONGO_HOST")
         return KMongo.createClient(host)
     }
 
     @Provides
+    @Singleton
     fun provideNewpornDb(client: MongoClient): MongoDatabase {
         return client.getDatabase("newporn")
     }
 
     @Provides
+    @Singleton
     fun providePostsDao(db: MongoDatabase): PostsDao {
         return PostsDao(db)
     }
 
     @Provides
+    @Singleton
     fun provideChannelsDao(db: MongoDatabase): ChannelsDao {
         return ChannelsDao(db)
     }
